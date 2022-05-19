3f3641f4e5ad93cbb4e59f631e69ff643a617d41
bryce <blarkin13@gmail.com>
2022-05-19 00:01:50 -0500

    idb logic

---
+++ public/js/idb.js
@@ -0,0 +1,26 @@
+let db;
+
+const request = indexedDB.open('budget_tracker', 1);
+// event will emit if the database version changes (ex. 1 to 2 or v1 to v2 etc..)
+request.onupgradeneeded = function(event) {
+    // save reference to the db
+    const db = event.target.result;
+    // create an object store (table) called 'new_budget', set to have auto increm primary key
+    db.createObjectStore('new_budget', { autoIncrement: true });
+};
+
+// upon a successful
+request.onsuccess = function(event) {
+    // when db is successfully created with its object store (from oneupgradeneeded event above)  or simply established a connection, save reference to db in global variable
+    db = event.target.result;
+
+    //  // check if app is online, if yes run uploadBudget() function to send all local db data to api
+    if (navigator.onLine) {
+        // uploadBudget();
+    }
+};
+
+request.onerror = function(event) {
+    // log error here
+    console.log(event.target.errorCode);
+};
