302a84aa7f09b574e1f8355d9ec19041c46d8fbb
bryce <blarkin13@gmail.com>
2022-05-19 00:18:37 -0500

    data wrtiting function structure

--- public/js/idb.js
+++ public/js/idb.js
@@ -24,3 +24,16 @@ request.onerror = function(event) {
     // log error here
     console.log(event.target.errorCode);
 };
+
+// This function will be executed if we attempt to submit a new pizza and there's no internet connection
+function saveRecord(record) {
+    // open new transaction with db with read/write permissions
+    const transaction = db.transaction(['new_budget'], 'readwrite');
+
+    // access the object store for 'new_budget'
+    const budgetObjectStore = transaction.objectStore('new_budget');
+
+    // add record to the store with add method
+
+    budgetObjectStore.add(record);
+}
